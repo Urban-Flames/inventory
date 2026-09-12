56fae9f848c8d5078a8953d502c28ef53d5b2ffb
destora-tech <destinyolaitan419@gmail.com>
2026-09-12 16:07:12 +0000

    updated

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -581,7 +581,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
 
 
-{/* EXPENSES SECTION */}
+{/* EXPENSES SECTIONs */}
 <SidebarGroup>
   <SidebarGroupLabel>Expenses</SidebarGroupLabel>
   <SidebarMenu>
